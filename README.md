002bac6865d60675cac469f148eee940c69a5675
Martin Stoffel <52819829+MartinStoffel@users.noreply.github.com>
2025-02-11 19:34:11 +0100

    4 get summary (#11)
    * Add summary sensors

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 Integration of Tigo into home assistant
 
 Currently quite basic, just integrates the panel data, since that is the tigo hardware I have. 
+
+
 ![image](https://github.com/MartinStoffel/tigo/assets/52819829/5e79c8e0-7f75-4466-b1e8-5c553c136822)
+
+Added current, houerly, daily, monthly and yearly production as well as all time production.
+
+![image](https://github.com/user-attachments/assets/62f184ae-28db-457a-8cbb-fed60282852d)
 
 
 Any feedback is welcome!
--- custom_components/tigo_cloud/sensor.py
+++ custom_components/tigo_cloud/sensor.py
@@ -1,5 +1,7 @@
 """Sensors for Tigo."""
 
+import contextlib
+
 from homeassistant.components.sensor import (
     SensorDeviceClass,
     SensorEntity,
@@ -86,6 +88,51 @@ PANEL_PROPERTIES = {
     },
 }
 
+SUMMARY_PROPERTIES = {
+    "now": {
+        "name": "Current Production",
+        "native_unit_of_measurement": UnitOfPower.WATT,
+        "device_class": SensorDeviceClass.POWER,
+        "state_class": SensorStateClass.MEASUREMENT,
+        "attr_icon": "mdi:solar-power-variant",
+    },
+    "hour": {
+        "name": "Hourly Production",
+        "native_unit_of_measurement": UnitOfEnergy.WATT_HOUR,
+        "device_class": SensorDeviceClass.ENERGY,
+        "state_class": SensorStateClass.TOTAL_INCREASING,
+        "attr_icon": "mdi:solar-power-variant-outline",
+    },
+    "day": {
+        "name": "Daily Production",
+        "native_unit_of_measurement": UnitOfEnergy.WATT_HOUR,
+        "device_class": SensorDeviceClass.ENERGY,
+        "state_class": SensorStateClass.TOTAL_INCREASING,
+        "attr_icon": "mdi:solar-power-variant-outline",
+    },
+    "month": {
+        "name": "Monthly Production",
+        "native_unit_of_measurement": UnitOfEnergy.WATT_HOUR,
+        "device_class": SensorDeviceClass.ENERGY,
+        "state_class": SensorStateClass.TOTAL_INCREASING,
+        "attr_icon": "mdi:solar-power-variant-outline",
+    },
+    "year": {
+        "name": "Yearly Production",
+        "native_unit_of_measurement": UnitOfEnergy.WATT_HOUR,
+        "device_class": SensorDeviceClass.ENERGY,
+        "state_class": SensorStateClass.TOTAL_INCREASING,
+        "attr_icon": "mdi:solar-power-variant-outline",
+    },
+    "allTime": {
+        "name": "All Time Production",
+        "native_unit_of_measurement": UnitOfEnergy.WATT_HOUR,
+        "device_class": SensorDeviceClass.ENERGY,
+        "state_class": SensorStateClass.TOTAL_INCREASING,
+        "attr_icon": "mdi:solar-power-variant-outline",
+    },
+}
+
 
 async def async_setup_entry(
     hass: HomeAssistant, config: ConfigType, add_entities: AddEntitiesCallback
@@ -95,17 +142,51 @@ async def async_setup_entry(
 
     entities = []
     for panel in coordinator.get_panels():
-        for key in PANEL_PROPERTIES.keys(): 
-            try:
+        for key in PANEL_PROPERTIES:
+            with contextlib.suppress(Exception):
                 entities.append(TigoPanelSensor(panel, coordinator, key))
-            except:
-                pass
+
+    for key in SUMMARY_PROPERTIES:
+        with contextlib.suppress(Exception):
+            entities.append(TigoSummarySensor(coordinator, key))
 
     add_entities(entities)
 
 
+class TigoSummarySensor(CoordinatorEntity, SensorEntity):
+    """The Tigo summary sensor, showing daily, monthly ans yearly stats."""
+
+    def __init__(self, coordinator, property) -> None:
+        """Initialize the sensor."""
+        super().__init__(coordinator)
+        self._property = property
+        self._attr_unique_id = DOMAIN + "tigo.summary." + property
+
+        self._attr_name = SUMMARY_PROPERTIES[property]["name"]
+        self._attr_native_unit_of_measurement = SUMMARY_PROPERTIES[property][
+            "native_unit_of_measurement"
+        ]
+        self._attr_device_class = SUMMARY_PROPERTIES[property]["device_class"]
+        self._attr_state_class = SUMMARY_PROPERTIES[property]["state_class"]
+        self._attr_icon = SUMMARY_PROPERTIES[property]["attr_icon"]
+        self._attr_suggested_display_precision = SUMMARY_PROPERTIES[property].get(
+            "suggested_display_precision"
+        )
+        self.update()
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        self.update()
+        self.async_write_ha_state()
+
+    def update(self) -> None:
+        """Update the data values."""
+        self._attr_native_value = self.coordinator.get_summary(self._property)
+
+
 class TigoPanelSensor(CoordinatorEntity, SensorEntity):
-    """THe Tigo panle sensor."""
+    """The Tigo panle sensor."""
 
     def __init__(self, panel, coordinator, property) -> None:
         """Initialize the sensor."""
